05a319eaf4540e20aacc14bb6190be7e99cda92e
Jaakko <t3maja06@students.oamk.fi>
2024-11-13 11:45:29 +0200

    main.cpp hienosäätöä

--- h3a/main.cpp
+++ h3a/main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main()
 {
+    Chef myChef("Gordon Ramsay");
+    myChef.makeSalad();
+    myChef.makeSoup();
     ItalianCheff myChefItalic("Anthony Bourdain");
     myChefItalic.makeSalad();
     myChefItalic.makeSoup();
